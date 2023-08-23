3a0d2af8eb502869386c80976ea605a5a30a0fd7
aakashdeveloper <ahanda205@gmail.com>
2023-08-23 07:55:39 +0530

    fsfile

---
+++ inbuilt/fs.js
@@ -0,0 +1,19 @@
+let fs = require('fs');
+
+// fs.writeFile('myFile.txt','My Next Time file for Fs',function(){
+//     console.log('File Created')
+// })
+
+// fs.appendFile('myCode.txt','This is line number \n',function(){
+//     console.log('File Appneded')
+// })
+
+// fs.readFile('data1.json','utf-8',function(err,data){
+//     if(err) throw err;
+//     console.log(data)
+// })
+
+fs.rename('myCode.txt','newFile.txt',function(err){
+    if(err) throw err
+    console.log('File Renamed')
+})
